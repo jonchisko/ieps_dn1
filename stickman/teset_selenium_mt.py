affbaad140e34012c57048eea1ed886ca034d1d3
DomenPozrl <domenpozrl1994@gmail.com>
2019-03-22 12:04:11 +0100

    spremenil delovanje. dodal class url pa site k hendlata robots.txt pa hranita vse podatke k jih bomo rabl za bazo i guess. cakam jenkota da se posvetujemo

--- stickman/teset_selenium_mt.py
+++ stickman/teset_selenium_mt.py
@@ -13,49 +13,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from multiprocessing import Queue
-
+from URLclasses import URL, Site
+from urllib.parse import urlparse
 import time
 import requests
-
-"""def th(bs):
-    rezultat = ""
-    for tag in bs.find_all('title'):
-        rezultat += tag.text
-    return rezultat
-
-
-urls = ["http://evem.gov.si", "http://podatki.gov.si", "http://e-prostor.gov.si"] #"http://e-uprava.gov.si"
-delo = []
-for url in urls:
-    options = Options()
-    options.headless = True
-
-    driver = webdriver.Chrome(options=options)
-    driver.get(url)
-    html = driver.page_source
-    driver.close()
-    soup = BeautifulSoup(html, "lxml")
-    delo.append(soup)
-
-    print(f"Stored {url}")
-
-
-q = Queue()
-threadlist = []
-print("printing")
-for i in range(len(urls)):
-
-    argument = delo[i]
-    t = Thread(target=lambda q, arg1: q.put(th(arg1)), args=(q, argument))
-    t.start()
-    threadlist.append(t)
-
-for th in threadlist:
-    th.join()
-
-while not q.empty():
-    print(q.get())"""
-
 
 class Crawler:
 
@@ -64,8 +25,11 @@ class Crawler:
         self.frontier = ["http://evem.gov.si", "http://e-uprava.gov.si", "http://podatki.gov.si", "http://e-prostor.gov.si"]
         self.q = Queue()
         self.workers = []
-        self.visited = set(["http://evem.gov.si", "http://e-uprava.gov.si", "http://podatki.gov.si", "http://e-prostor.gov.si"])
+        self.visited = set()
+        self.sites = dict()
 
+    def check_for_existing_site(self, url):
+        return urlparse(url).netloc in self.sites
 
     def start_crawling(self):
         iii = 5
@@ -77,15 +41,10 @@ class Crawler:
                 t.start()
                 self.workers.append(t)
 
-            #TODO: Delete this when you are certain that the above for loop works
-            """for unprocessed in self.waiting_for_processing[:self.threadnum]:
-                t = Thread(target=self.store_processed_to_queue(unprocessed), args=(unprocessed))
-                t.start()
-                self.workers.append(t)"""
-
             #wait for workers to finish
             for thread in self.workers:
                 thread.join()
+
             print(f"The length of the frontier is {len(self.frontier)}")
             #we have to remove the visited sites from the frontier
             #we also have to remove the stored bs for sites
@@ -101,7 +60,7 @@ class Crawler:
 
                 original_url, links, files, images = self.q.get()
 
-                print(f"Adding links from {original_url} to frontier:")
+                print(f"Adding links from {original_url.url} to frontier:")
                 for link in links:
                     if link not in self.visited:
                         self.visited.add(link)
@@ -109,19 +68,62 @@ class Crawler:
                         self.frontier.append(link)
 
             iii -= 1
+    """
+    class Site:
 
+    def __init__(self, allow, disallow, sitemap, delay, site_id):
+        self.allow = allow
+        self.disallow = disallow
+        self.sitemap = sitemap
+        self.delay = delay
+        self.site_id = site_id
+
+class URL:
+
+    def __init__(self, url, allow, disallow, delay, site_id): #, page_id, html):
+        self.url = url
+        self.domain = urlparse(url).netloc
+        self.acces_time = time.time()
+        self.allow = allow
+        self.disallow = disallow
+        self.delay = delay
+        self.site_id = site_id
+        self.page_id = None
+        self.html = None
+    
+    """
     def store_processed_to_queue(self, url):
 
         # set options for headless browsing
-        print(f"Working for {url}")
         options = Options()
         options.headless = True
 
-        # check for robots.txt and parse if exists
-        rh = RobotsTxtHandler(url)
+        current_domain = urlparse(url).netloc
+
+        #ce mamo ze site za ta page ne rabimo iskat robotsov itd
+        #samo nrdimo instanco page-a
+        if self.check_for_existing_site(url):
+            url_class_instance = URL(url, self.sites[current_domain].allow, self.sites[current_domain].disallow, self.sites[current_domain].delay, self.sites[current_domain].site_id)
+            print(f"Working for {url}: \n        We already know this domain, we have to avoid {len(self.sites[current_domain].disallow)} urls\n        We can visit {len(self.sites[current_domain].allow)} sites\n        We have to obey the crawl delay which is {self.sites[current_domain].delay}\n")
+        else:
+            #v tem primeru pa se nimamo site in je treba narest najprej treba dobit robotse za domeno strani
+            rh = RobotsTxtHandler("http://" + current_domain)
+            allow, disallow, sitemap = rh.allow, rh.disallow, rh.sitemap
+
+            #instancirat domeno strani
+            site = Site(allow, disallow, sitemap, rh.crawl_delay, None)
+
+            #dodat site v vse site
+            self.sites[current_domain] = site
+
+            #narest se class url za obdelavo
+            url_class_instance = URL(url, self.sites[current_domain].allow, self.sites[current_domain].disallow, self.sites[current_domain].delay, self.sites[current_domain].site_id)
+            print(f"Working for {url}: \n        This is the first time visiting this domain, we have to avoid {len(self.sites[current_domain].disallow)} urls\n        We can visit {len(self.sites[current_domain].allow)} sites\n        We have to obey the crawl delay which is {self.sites[current_domain].delay}\n")
+
+
 
         # sleep for the crawl_delay, default 4s
-        time.sleep(rh.crawl_delay)
+        time.sleep(url_class_instance.delay)
 
         # set up the driver
         driver = webdriver.Chrome(options=options)
@@ -129,6 +131,7 @@ class Crawler:
         # fetch html
         driver.get(url)
         html = driver.page_source
+        url_class_instance.set_html(html)
 
         # close driver
         driver.close()
@@ -139,7 +142,7 @@ class Crawler:
         # store html to soup and save it for multithreaded processing
         soup = BeautifulSoup(html, "lxml")
 
-        self.q.put(self.parsePage(url, soup, rh.disallow))
+        self.q.put(self.parsePage(url_class_instance, soup))
 
         print(f"Done working for {url}")
 
@@ -150,10 +153,12 @@ class Crawler:
         return rezultat
 
     # en process soup, k sam sparsa - dodal user: jon skoberne
-    def parsePage(self, url, soup, robots):
+    def parsePage(self, url_class_instance, soup):
+        url = url_class_instance.url
+        robots = url_class_instance.disallow
         urlSet, fileSet, imgSet = htmlGetAll.doPage(url, soup, robots)
         #TODO: check if unique urls, insert files, imgs into db
-        return url, urlSet, fileSet, imgSet
+        return url_class_instance, urlSet, fileSet, imgSet
 
     # en process soup, k tud geta pa sparsa - dodal user: jon skoberne
     def getParsePage(self, url):

df1b3e8fb44b8a72442ef2a56ef6b496be70c5f6
DomenPozrl <40770999+DomenPozrl@users.noreply.github.com>
2019-04-02 22:03:42 +0200

    Add files via upload

---
+++ crawler/Crawler.py
@@ -0,0 +1,355 @@
+import os
+
+if os.name == "nt":
+    from pars import *
+    from robots_test import *
+elif os.name == "posix":
+    from parser.pars import *
+    from stickman.robots_test import *
+
+from threading import Thread
+import urllib.request
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+from multiprocessing import Queue
+from URLclasses import URL, Site
+from urllib.parse import urlparse
+import time
+import requests
+from database import Database
+import getFile
+import hashlib
+
+class Crawler:
+
+    def __init__(self, seeds, number_of_workers):
+        self.threadnum = number_of_workers
+        self.seeds = seeds
+        self.frontier = []
+        self.q = Queue()
+        self.workers = []
+        self.visited = set()
+        self.content = dict()
+        self.sites = dict()
+        self.db = Database()
+        self.maxPageID, self.maxSiteID = self.db.getMaxIDs()
+
+    def check_for_existing_site(self, url):
+        # urlparese(url).netloc vrne domeno
+        return urlparse(url).netloc in self.sites
+
+
+    #This functions has to be called before we start crawling. It takes the seed urls and parses them and inserts them into the frontier
+    def setup_crawler(self):
+        print("Setting up the crawler by adding seeds to the frontier.")
+
+        self.visited = self.db.getVisited()
+        self.content = self.db.getContentHash()
+        db_forntier = self.db.getFrontier()
+
+        # If the database froniter is empty, add the seeds to it and the RAM froniter
+        if len(db_forntier) == 0:
+            for url in self.seeds:
+                # Make a class instance from string url (adds domain info, such as robots.txt and sitemap)
+                url_class_instance = self.domain_checking(url)
+                self.frontier.append(url_class_instance)
+                self.visited.add(url)
+
+        else:
+            for page in db_forntier:
+                p1 = page[4].find('Disallow: ')
+                p2 = page[4].find('Delay: ')
+
+                allows = page[4][:p1]
+                disallows = page[4][p1:p2]
+                delay = int(page[4][p2:].replace('Delay: ', ''))
+
+                url_class_instance = URL(page[2], allows, disallows, delay, page[1], page[0])
+                site = Site(page[3], allows, disallows, page[5], delay, page[1])
+
+                self.sites[page[3]] = site
+                self.frontier.append(url_class_instance)
+
+    def start_crawling(self):
+        while self.frontier:
+
+            #launch workers
+            workers = self.threadnum if len(self.frontier) >= self.threadnum else len(self.frontier)
+            for url_class_instance in self.frontier[:workers]:
+                t = Thread(target=self.store_processed_to_queue, args=(url_class_instance,))
+                t.start()
+                self.workers.append(t)
+
+            #wait for workers to finish
+            for thread in self.workers:
+                thread.join()
+
+            print(f"The length of the frontier is {len(self.frontier)}")
+            # Remove visited sites from the frontier
+            for i in range(workers):
+                del self.frontier[0]
+
+
+            print("            Pobiram rezultat               ")
+            #print out the results of crawling on lvl 1
+            #the parser returns (url, set(urls), set(files), set(images))
+            #now we have to empty the q, store stuff to the database in feed everything to the frontier correctly
+            while not self.q.empty():
+                original_url, links, files, images = self.q.get()
+
+                hash = hashlib.md5(original_url.html.encode('utf-8')).hexdigest()
+                duplicate = hash in self.content
+
+                if not duplicate:
+                    self.content[hash] = original_url.page_id
+
+                self.db.updateFroniter(original_url.page_id, original_url.html, hash, original_url.html_status_code,
+                                           original_url.time, duplicate)
+
+
+                print(f"Adding links from {original_url.url} to frontier:")
+
+                fromPage = []
+                toPage = []
+                for link in links:
+                    #preverja ce smo ze obiskali tocno tak url
+                    #ce se nismo ga uzamemo, instanciramo url_class_instance in ga dodamo v bazo in na frontier
+
+                    if link not in self.visited:
+                        #TODO: Content check, samo to lahko naredis sele ko ze imas content, torej v obdelavi original _urlja
+
+                        self.visited.add(link)
+                        print(f"   - added: {link}")
+
+                        #instanciran + doda page in site v DB + doda site v RAM
+                        url_class_instance = self.domain_checking(link)
+                        #dodan na frontier
+                        self.frontier.append(url_class_instance)
+
+                        # Record all the links
+                        fromPage.append(original_url.page_id)
+                        toPage.append(url_class_instance.page_id)
+
+                # Write links to DB
+                if len(fromPage) > 0:
+                    self.db.insert('link', {'from_page':fromPage, 'to_page':toPage})
+
+
+                for image in images:
+                    if image not in self.visited:
+                        response = getFile.GetFileFromURL.get_file_from_url(image)
+
+                        if not response:
+                            continue
+
+                        fileName, imgType, statusCode = response
+
+                        self.visited.add(image)
+                        self.addFileToDB(image, 'img', fileName, imgType, statusCode)
+                        getFile.GetFileFromURL.delete_file_from_disc(fileName)
+                        print(f"   - image: {fileName}")
+
+                for file in files:
+                    type = file.split('.')[-1]
+
+                    if type not in ['pdf', 'doc', 'docx', 'ppt', 'pptx']:
+                        continue
+
+                    if file not in self.visited:
+                        response = getFile.GetFileFromURL.get_file_from_url(file)
+
+                        if not response:
+                            continue
+
+                        fileName, fileType, statusCode = response
+
+                        self.visited.add(file)
+                        self.addFileToDB(file, 'doc', fileName, fileType, statusCode)
+                        getFile.GetFileFromURL.delete_file_from_disc(fileName)
+                        print(f"   - file: {fileName}")
+
+
+    """
+    class Site:
+
+    def __init__(self, allow, disallow, sitemap, delay, site_id):
+        self.allow = allow
+        self.disallow = disallow
+        self.sitemap = sitemap
+        self.delay = delay
+        self.site_id = site_id
+
+class URL:
+
+    def __init__(self, url, allow, disallow, delay, site_id): #, page_id, html):
+        self.url = url
+        self.domain = urlparse(url).netloc
+        self.acces_time = time.time()
+        self.allow = allow
+        self.disallow = disallow
+        self.delay = delay
+        self.site_id = site_id
+        self.page_id = None
+        self.html = None
+    
+    """
+
+    def addFileToDB(self, url, type, fileName, fileType, statusCode):
+        domain = urlparse(url).netloc
+        self.maxPageID += 1
+
+        if self.check_for_existing_site(url):
+            siteID = self.sites[domain].site_id
+
+        else:
+            rh = RobotsTxtHandler("http://" + domain)
+            allow, disallow, sitemap = rh.allow, rh.disallow, rh.sitemap
+            self.maxSiteID += 1
+            site = Site(url, allow, disallow, sitemap, rh.crawl_delay, self.maxSiteID)
+            allows, disallows, sitemap = site.get_robots_strings()
+            self.db.insert('site', {'id': [self.maxSiteID],
+                                    'domain': [site.domain],
+                                    'robots_content': [allows + disallows + 'Delay: ' + str(site.delay)],
+                                    'sitemap_content': [sitemap]})
+
+            self.sites[domain] = site
+            siteID = site.site_id
+
+        try:
+            if type == 'img':
+                self.db.insertImage(url, siteID, self.maxPageID, fileName, fileType, statusCode)
+            elif type == 'doc':
+                self.db.insertFile(url, siteID, self.maxPageID, fileName, fileType, statusCode)
+        except:
+            # Vcasih pac ne zloada fajla, in pol nimas kej
+            pass
+
+
+    #Preveri ce smo domeno ze shranili
+    #Ce je nismo jo shrani lokalno in v bazo doda site
+    def domain_checking(self, url):
+
+        current_domain = urlparse(url).netloc
+        self.maxPageID += 1
+
+        # ce mamo ze site za ta page ne rabimo iskat robotsov itd
+        # samo nrdimo instanco page-a
+        if self.check_for_existing_site(url):
+            siteID = self.sites[current_domain].site_id
+
+            url_class_instance = URL(url, self.sites[current_domain].allow, self.sites[current_domain].disallow,
+                                     self.sites[current_domain].delay, siteID, self.maxPageID)
+
+            self.db.insert('page', {'id': [self.maxPageID],
+                                    'site_id': [siteID],
+                                    'page_type_code': ['FRONTIER'],
+                                    'url': [url]})
+
+        else:
+            # v tem primeru pa se nimamo site in je treba narest najprej treba dobit robotse za domeno strani
+            rh = RobotsTxtHandler("http://" + current_domain)
+            allow, disallow, sitemap = rh.allow, rh.disallow, rh.sitemap
+
+            # instancirat domeno strani
+            self.maxSiteID += 1
+            site = Site(url, allow, disallow, sitemap, rh.crawl_delay, self.maxSiteID)
+
+            allows, disallows, sitemap = site.get_robots_strings()
+            self.db.insert('site', {'id': [self.maxSiteID],
+                                    'domain':[site.domain],
+                                    'robots_content':[allows + disallows + 'Delay: ' + str(site.delay)],
+                                    'sitemap_content': [sitemap]})
+
+            self.sites[current_domain] = site
+
+            self.db.insert('page', {'id': [self.maxPageID],
+                                    'site_id':[self.maxSiteID],
+                                    'page_type_code':['FRONTIER'],
+                                    'url':[url]})
+
+            url_class_instance = URL(url, site.allow, site.disallow, site.delay, site.site_id, self.maxPageID)
+
+        return url_class_instance
+
+    def store_processed_to_queue(self, url_class_instance):
+        # set options for headless browsing
+        options = Options()
+        options.headless = True
+
+        # sleep for the crawl_delay, default 4s
+        time.sleep(url_class_instance.delay)
+
+        # set up the driver
+        driver = webdriver.Chrome(options=options)
+
+        #get the status code
+        try:
+            r = requests.get(url_class_instance.url)
+            url_class_instance.set_html_status_code(int(r.status_code))
+        except:
+            url_class_instance.set_html_status_code(404)
+
+        if url_class_instance.html_status_code != 200:
+            self.db.updateFroniter(url_class_instance.page_id, None, None, url_class_instance.html_status_code, int(time.time()), False)
+            print("Error loading page, not working with it")
+            return
+
+        # fetch html
+        driver.get(url_class_instance.url)
+        html = driver.page_source
+        url_class_instance.set_html(html)
+        url_class_instance.set_time(int(time.time()))
+
+
+        # close driver
+        driver.close()
+
+        # add url to visited
+        self.visited.add(url_class_instance.url)
+
+        # store html to soup and save it for multithreaded processing
+        soup = BeautifulSoup(html, "lxml")
+
+        self.q.put(self.parsePage(url_class_instance, soup))
+
+        print(f"Done working for {url_class_instance.url}")
+
+    def process_soup(self, soup):
+        rezultat = []
+        for tag in soup.find_all('title'):
+            rezultat.append(tag.text)
+        return rezultat
+
+    # en process soup, k sam sparsa - dodal user: jon skoberne
+    def parsePage(self, url_class_instance, soup):
+        url = url_class_instance.url
+        robots = url_class_instance.disallow
+        urlSet, fileSet, imgSet = htmlGetAll.doPage(url, soup, robots)
+        #TODO: check if unique urls, insert files, imgs into db
+        return url_class_instance, urlSet, fileSet, imgSet
+
+    # en process soup, k tud geta pa sparsa - dodal user: jon skoberne
+    def getParsePage(self, url):
+        #TODO:everything
+        # urllib request apparently works in threads, so this is for threads
+        response = requests.get(url, verify=False, allow_redirects=True, timeout=50)
+        soup = BeautifulSoup(response.text, "lxml")
+        robots = []
+        urlSet, fileSet, imgSet = htmlGetAll.doPage(url, soup, robots)
+        # TODO: status codes, check if unique urls, insert files, imgs into db
+
+    # function checks if page needs selenium
+    def checkPage(self, url):
+        # get http head
+        # head = requests.head(url)
+        pass
+
+    # get page
+    def getPage(self, url):
+        pass
+
+crw = Crawler(["http://evem.gov.si", "http://e-uprava.gov.si", "http://podatki.gov.si", "http://e-prostor.gov.si"], 4)
+crw.setup_crawler()
+crw.start_crawling()
+
---
+++ crawler/robots_test.py
@@ -0,0 +1,84 @@
+import requests
+import urllib.robotparser
+
+"""
+For each domain respect the robots.txt file if it exists. Correctly respect the commands User-agent, Allow, Disallow, 
+Crawl-delay and Sitemap. If a sitemap is defined, all the URLs that are defined within it, should be added to the frontier. 
+Make sure to respect robots.txt as sites that define special crawling rules often contain spider traps.
+
+"""
+
+#Function parses the robots.txt file of a site and returns a list of link in the sitemap,
+#list of disallowed postfixes and the crawl delay. If nothing is found then the lists are empty and the crawl delay is 4
+def parse_robots_sitemap(text):
+    sitemap = []
+    for line in  text.split("\n"):
+        if "Sitemaps: " in line:
+            sitemap.append(line.split("Sitemap: ")[1])
+    return sitemap
+
+def parse_robots_allows(url):
+    allows = []
+    disallows = []
+    rp = urllib.robotparser.RobotFileParser()
+    rp.set_url(url)
+    rp.read()
+
+    if rp.default_entry:
+        for rule in rp.default_entry.rulelines:
+            if rule.allowance:
+                allows.append(rule.path)
+            else:
+                disallows.append(rule.path)
+        if rp.default_entry.delay:
+            crawl_delay = int(rp.default_entry.delay)
+        else:
+            crawl_delay = 4
+    else:
+        crawl_delay = 4
+        allows = []
+        disallows = []
+
+    return crawl_delay, allows, disallows
+
+#this class is used to parse the robots.txt files for each site
+class RobotsTxtHandler:
+
+    def __init__(self, link):
+        #add /robots.txt to link if needed
+        self.link = link + "/robots.txt" if "robots.txt" not in link else link
+        self.crawl_delay = 4
+        self.allow = []
+        self.disallow = []
+        self.sitemap = []
+        #fetch the page
+        try:
+            self.response = requests.get(self.link)
+            # if the page exist and is accessible then parse the file and set the class attributes accordingly
+
+            try:
+                self.response.raise_for_status()
+                self.sitemap = parse_robots_sitemap(self.response.text)
+                self.robots_exists = True
+
+            # if raise for status triggers and exception assume that there is no robots.txt file and set attributes to default values
+            except Exception as e:
+                # print(f"Exception was triggered while fetching: {e}")
+                self.sitemap, self.allow, self.disallow, self.crawl_delay = [], [], [], 4
+                self.robots_exists = False
+        except:
+            self.sitemap, self.allow, self.disallow, self.crawl_delay = [], [], [], 4
+            self.robots_exists = False
+
+
+
+        #if the self.robots_exists flag has been set then the try clause didn't trigger an exception and the sitemap is set
+        #we now have to get the delay and allow and disallow
+        if self.robots_exists:
+            self.crawl_delay, self.allow, self.disallow = parse_robots_allows(self.link)
+        self.crawl_delay = int(self.crawl_delay)
+
+
+
+if __name__ == "__main__":
+    rth = RobotsTxtHandler("https://www.thetimes.co.uk/")
